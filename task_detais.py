3973f31fe4fac86e65fbfdc4acb2afd929b31e47
Redowan Pottro <redowanpottro@gmail.com>
2025-11-18 15:53:07 +0600

    v1

---
+++ task_detais.py
@@ -0,0 +1,18 @@
+tasks=[]
+def add_task(tasks):
+    Title=input("Enter the title of you task: ")
+    Date=input("Enter due date(dd/mm/yyyy)")
+    Priority=input("Enter priority level (Critical/High/Medium/Low)")
+
+    task = {
+        "Title" : Title,
+        "Due" : Date if Date else "N/A",
+        "Priority" : Priority,
+    }
+
+    tasks.append(task)
+    print(f"Task'{Title}' added")
+
+'''check '''   
+add_task(tasks)
+print(tasks)
