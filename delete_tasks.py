916047f43f3bcbc37c4a32f072bd5b9196c99b90
Redowan Pottro <redowanpottro@gmail.com>
2025-11-19 14:43:39 +0600

    v1

--- delete_tasks.py
+++ delete_tasks.py
@@ -1,18 +1,14 @@
-# import the function from necessary files
+
 from view_tasks import view
 from task_details import tasks
-# Create a function
 
 def delete(tasks):
-# what the funtion does when target not in the list
             if not tasks:
                     print("No tasks to delete")
                     return
-#show all stored tasks
-#             
+             
             view(tasks)
 
-# what the function will do when target in  list
             n=int(input("Enter the number of the task you want to delete: "))
             if n < 0 or n > len(tasks):
                     print("Invalid task number. Try again")
